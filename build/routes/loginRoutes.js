a453e889d746b00ca482f8c0ce9c8e6a9b55f891
Nimira43 <n.rabone72@aol.com>
2024-11-14 23:13:33 +0000

    Basic Login form made

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -5,5 +5,19 @@ const express_1 = require("express");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/login', (req, res) => {
-    res.send('GateKeeper!');
+    res.send(`
+    <form method='POST'>
+      <h1>Gatekeeper</h1>
+      <h3>Login to Account</h3>
+      <div>
+        <label>Email</label>
+        <input name='email' />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name='password' type='password' />
+      </div>
+      <button>Submit</button>
+    </form>
+    `);
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -3,7 +3,23 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.get('/login', (req: Request, res: Response) => {
-  res.send('GateKeeper!')
+  res.send(
+    `
+    <form method='POST'>
+      <h1>Gatekeeper</h1>
+      <h3>Login to Account</h3>
+      <div>
+        <label>Email</label>
+        <input name='email' />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name='password' type='password' />
+      </div>
+      <button>Submit</button>
+    </form>
+    `    
+  )
 })
 
 export { router }
