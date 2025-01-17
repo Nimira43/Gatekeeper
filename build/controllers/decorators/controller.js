5b913ff5163c4391f9d6329ec8678e7e9107c2aa
Nimira43 <n.rabone72@aol.com>
2025-01-17 05:06:02 +0000

    Imported reflect-metadata and commented out ES5 related code in controllers.ts

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = controller;
+require("reflect-metadata");
 function controller(routePrefix) {
     return function (target) {
         // ES5
-        for (let key in target.prototype) {
-            const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata('path', target.prototype, key);
-        }
+        // for (let key in target.prototype) {
+        //   const routeHandler = target.prototype[key]
+        //   const path = Reflect.getMetadata('path', target.prototype, key)
+        // }
         // ES2016
         Object.getOwnPropertyNames(target.prototype).forEach((key) => {
             const routeHandler = target.prototype[key];
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata'
+
 export function controller(routePrefix : string) {
   return function(target: Function) {
     // ES5
-    for (let key in target.prototype) {
-      const routeHandler = target.prototype[key]
-      const path = Reflect.getMetadata('path', target.prototype, key)
-    }
+    // for (let key in target.prototype) {
+    //   const routeHandler = target.prototype[key]
+    //   const path = Reflect.getMetadata('path', target.prototype, key)
+    // }
     // ES2016
     Object.getOwnPropertyNames(target.prototype).forEach((key) => {
       const routeHandler = target.prototype[key]
