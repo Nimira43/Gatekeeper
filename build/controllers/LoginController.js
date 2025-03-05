9dfd1ed92916dc936478ae06ff865713aee66a45
Nimira43 <n.rabone72@aol.com>
2025-03-05 22:33:31 +0000

    Brought in controller from controller.ts

--- build/controllers/LoginController.js
+++ build/controllers/LoginController.js
@@ -10,6 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const routes_1 = require("./decorators/routes");
+const controller_1 = require("./decorators/controller");
 let LoginController = class LoginController {
     getLogin(req, res) {
         res.send(`
@@ -36,5 +37,5 @@ __decorate([
     __metadata("design:returntype", void 0)
 ], LoginController.prototype, "getLogin", null);
 LoginController = __decorate([
-    Controller('/')
+    (0, controller_1.controller)('/auth')
 ], LoginController);
