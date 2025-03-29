2300e0f3969434cb99a79daad32be5f242f5d6cb
Nimira43 <n.rabone72@aol.com>
2025-03-29 21:21:51 +0000

    Refined implementation of decorators leveraging reflect-metadata. Expanded it by adding both path and method metadata, distinguishing between GET and POST operations.

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.get = get;
+exports.post = post;
 require("reflect-metadata");
 function get(path) {
     return function (target, key, desc) {
         Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'get', target, key);
+    };
+}
+function post(path) {
+    return function (target, key, desc) {
+        Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'post', target, key);
     };
 }
