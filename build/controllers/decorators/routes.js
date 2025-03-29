caa2c28818028e6eeaf2cab8e3def3f24278c239
Nimira43 <n.rabone72@aol.com>
2025-03-29 21:27:25 +0000

    Scalable approach with the routeBinder function to dynamically handle HTTP methods and paths by centralising logic in routeBinder.

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = get;
-exports.post = post;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
-        Reflect.defineMetadata('method', 'get', target, key);
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', 'get', target, key);
+        };
     };
 }
-function post(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
-        Reflect.defineMetadata('method', 'post', target, key);
-    };
-}
+exports.get = routeBinder('get');
+exports.put = routeBinder('put');
+exports.post = routeBinder('post');
+exports.del = routeBinder('get');
+exports.patch = routeBinder('patch');
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata'
 
-export function get(path: string) {
-  return function(target: any, key: string, desc: PropertyDescriptor)  {
-    Reflect.defineMetadata('path', path, target, key)
-    Reflect.defineMetadata('method', 'get', target, key)
+function routeBinder(method: string) {
+  return function(path: string) {
+    return function(target: any, key: string, desc: PropertyDescriptor)  {
+      Reflect.defineMetadata('path', path, target, key)
+      Reflect.defineMetadata('method', 'get', target, key)
+    }
   }
 }
 
-export function post(path: string) {
-  return function(target: any, key: string, desc: PropertyDescriptor)  {
-    Reflect.defineMetadata('path', path, target, key)
-    Reflect.defineMetadata('method', 'post', target, key)
-  }
-}
+export const get = routeBinder('get')
+export const put = routeBinder('put')
+export const post = routeBinder('post')
+export const del = routeBinder('get')
+export const patch = routeBinder('patch')
+
+
