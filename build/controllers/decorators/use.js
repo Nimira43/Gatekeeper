2f3f87cc58a90f0f374da8ca33b32cfaaf8bd269
Nimira43 <n.rabone72@aol.com>
2025-04-20 01:08:03 +0100

    Introducing a decorator to apply middleware to your methods dynamically

--- build/controllers/decorators/use.js
+++ build/controllers/decorators/use.js
@@ -1,3 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.use = use;
 require("reflect-metadata");
+function use(middleware) {
+    return function (target, key, desc) {
+    };
+}
--- src/controllers/decorators/use.ts
+++ src/controllers/decorators/use.ts
@@ -1,2 +1,9 @@
 import 'reflect-metadata'
 import { MetadataKeys } from './MetadataKeys'
+import { RequestHandler } from 'express'
+
+export function use(middleware: RequestHandler) {
+  return function (target: any, key: string, desc: PropertyDecorator) {
+    
+  }
+}
