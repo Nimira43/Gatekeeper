4b6c7559bc6f2403e02e9acfc6c94b712c262c60
Nimira43 <n.rabone72@aol.com>
2025-06-22 01:59:11 +0100

    bodyValidator - 2

--- build/controllers/decorators/bodyValidator.js
+++ build/controllers/decorators/bodyValidator.js
@@ -2,5 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bodyValidator = bodyValidator;
 require("reflect-metadata");
+var MetadataKeys_1 = require("./MetadataKeys");
 function bodyValidator() {
+    var keys = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        keys[_i] = arguments[_i];
+    }
+    return function (target, key, desc) {
+        Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.validator, keys, target, key);
+    };
 }
